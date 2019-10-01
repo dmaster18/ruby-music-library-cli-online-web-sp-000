3ac7de9a60b555e028985e02d88075924977d2f8
David Strauch <dmaster18@gmail.com>
2019-10-01 17:22:13 +0000

    Automatically backed up by Learn

--- lib2/musiclibrarycontroller.rb
+++ lib2/musiclibrarycontroller.rb
@@ -88,7 +88,6 @@ class MusicLibraryController
     if plays >= 1 && plays < list_songs.count
       artist = list_songs[plays][0]
       song = list_songs[plays][1]
-      
       puts "Playing #{"
  
     else
