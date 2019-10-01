a7cd4102e88f45f7f54ec429b4c4958be64cb595
David Strauch <dmaster18@gmail.com>
2019-10-01 17:49:15 +0000

    Automatically backed up by Learn

--- lib2/musiclibrarycontroller.rb
+++ lib2/musiclibrarycontroller.rb
@@ -82,7 +82,7 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     user_input = gets.chomp.to_s
     song_by_artist = @songs.find{|song| song.artist.name == user_input}
-    if 
+    if song_by_artist
     artist = song_by_artist.artist
     songs_by_artist = @songs.select{|song| song.artist == artist}
     i = 1
