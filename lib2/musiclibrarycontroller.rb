553ce64afa8a17ac89358a5da3943f5f0bd8f85e
David Strauch <dmaster18@gmail.com>
2019-10-01 17:22:21 +0000

    Automatically backed up by Learn

--- lib2/musiclibrarycontroller.rb
+++ lib2/musiclibrarycontroller.rb
@@ -88,7 +88,7 @@ class MusicLibraryController
     if plays >= 1 && plays < list_songs.count
       artist = list_songs[plays][0]
       song = list_songs[plays][1]
-      puts "Playing #{
+      puts "Playing #{song} by 
  
     else
       "Invalid song selection."
