4d3d0c530474a0211869ee65e577dd7a01da8a91
David Strauch <dmaster18@gmail.com>
2019-10-01 17:40:20 +0000

    Automatically backed up by Learn

--- lib2/musiclibrarycontroller.rb
+++ lib2/musiclibrarycontroller.rb
@@ -81,7 +81,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     user_input = gets.chomp.to_s
-    
+    @songs.
   end
   
   def list_artist
